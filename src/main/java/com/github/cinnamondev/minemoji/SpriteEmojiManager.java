64ee9a5534a1455b4acec70838e0b0d8c0947744
cinnamon <justpromptiguess@gmail.com>
2026-03-23 16:31:03 +0000

    Minemoji 1.3:
    - Downloaded / Remote packs now will redirect (to fix issue with default twemoji pack; please change URL from "cinnamondev.github.io" to "itscinnamon.dev" or update to this version.
    - Removed having to choose between downloaded/remote packs; you can now have both. Just drop pack.json files in minemoji/packs
    - Compiles against Paper 1.21.11 and DiscordSRV 1.30.4 now. I don't think this should have significant effect.
    - EmojiSets and SpriteEmojiManager (in particular) have been rewritten and there is now a (somewhat) more consistent way to interact with emojis. less relying on the config file everywhere.
      - its just a bit easier to look at working on. Emojis from EmojiManager translate straight into sprites rather than having strange maps for each possible format, etc... Or finding by family + name, name, or just Key.
    - The command tree is now different - though things such as /minemoji list <pack> <page> have turned into /minemoji pack <pack> list, they now show all emotes at once (exception: Unicode emotes, though these are still shaky usage.)
    - Removed 'discordsrv hack'

--- src/main/java/com/github/cinnamondev/minemoji/SpriteEmojiManager.java
+++ src/main/java/com/github/cinnamondev/minemoji/SpriteEmojiManager.java
@@ -1,17 +1,16 @@
 package com.github.cinnamondev.minemoji;
 
 import com.google.gson.Gson;
-import net.fellbaum.jemoji.Emoji;
+import com.google.gson.JsonSyntaxException;
+import github.scarsz.discordsrv.dependencies.commons.lang3.StringUtils;
+import github.scarsz.discordsrv.dependencies.emoji.Emoji;
 import net.fellbaum.jemoji.EmojiManager;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.ObjectComponent;
 import net.kyori.adventure.text.TextReplacementConfig;
-import net.kyori.adventure.text.object.ObjectContents;
+import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.object.SpriteObjectContents;
-import org.apache.commons.collections4.BidiMap;
-import org.apache.commons.collections4.bidimap.DualHashBidiMap;
-import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -21,6 +20,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
@@ -29,23 +29,22 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SpriteEmojiManager {
-    private final Minemoji p;
-    private final BidiMap<Emoji,ObjectComponent> emojiMap;
-    private final Map<String, EmojiSet> customPacks;
-    private final BidiMap<String,Key> customSprites;
-    private final Map<Key, String> customSpriteDiscord;
+    private static final String DEFAULT_UNICODE_PACK = "https://cinnamondev.github.io/minemoji/packs/twemoji-latest.zip";
+    private Minemoji p = null;
 
-    public static CompletableFuture<SpriteEmojiManager> fromRemotePacks(Minemoji p, List<URI> packs) {
-        p.getLogger().info("Creating emoji manager using remote packs");
+    public UnicodeEmojiSet unicodeEmojiSet = null;
+
+    public static CompletableFuture<Map<String, CustomEmojiSet>> downloadPacks(Minemoji p, List<URI> packs) {
         Gson gson = new Gson();
-        try (HttpClient client = HttpClient.newHttpClient()) {
-            List<CompletableFuture<EmojiSet>> futures = packs.stream()
+        try (HttpClient client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build()) {
+            List<CompletableFuture<CustomEmojiSet>> futures = packs.stream()
+                    .peek(uri -> p.getLogger().info("Remote Pack:" + uri))
                     .map(pack -> HttpRequest.newBuilder(pack).GET().build())
                     .map(request -> client
                             .sendAsync(request, HttpResponse.BodyHandlers.ofInputStream())
                             .thenApply(HttpResponse::body)
                             .thenApply(InputStreamReader::new)
-                            .thenApply(stream -> gson.fromJson(stream, EmojiSet.class))
+                            .thenApply(stream -> gson.fromJson(stream, CustomEmojiSet.class))
                     ).toList();
 
             return CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]))
@@ -55,116 +54,82 @@ public class SpriteEmojiManager {
                         try {
                             return Stream.of(f.join());
                         } catch (CompletionException e) {
-                            p.getLogger().warning("Malformed pack, ignoring... See message");
+                            p.getLogger().warning("Malformed pack ignoring... (download) See message");
                             p.getLogger().warning(ex.getMessage());
                             return Stream.empty();
                         }
                     }))
                     .thenApply(s -> s.map(set -> Map.entry(set.prefix, set))
                             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
-                    ).thenApply(map -> new SpriteEmojiManager(p, map));
+                    );
         }
     }
 
-    public static SpriteEmojiManager fromLocalPacks(Minemoji p, File packsDirectory) {
-        p.getLogger().info("Creating emoji manager using local packs");
-        return new SpriteEmojiManager(p, discoverEmojiPacks(p, packsDirectory));
-    }
-
-    private static Map<String, EmojiSet> discoverEmojiPacks(Minemoji p, File packsDirectory) {
-        HashMap<String, EmojiSet> map = new HashMap<>();
+    public static Map<String, CustomEmojiSet> fromLocalFilesystem(Minemoji p, File jsonDirectory) {
+        HashMap<String, CustomEmojiSet> emoteSet = new HashMap<>();
         Gson gson = new Gson();
-        if (!packsDirectory.exists()) {
-            packsDirectory.mkdirs();
-            p.getLogger().info("Creating packs directory (no packs available): " + packsDirectory.getAbsolutePath());
+
+        if (!jsonDirectory.exists()) {
+            jsonDirectory.mkdirs();
+            p.getLogger().warning("No pack directory was created, I've created one for you at " + jsonDirectory.getAbsolutePath());
             return Collections.emptyMap();
         }
-
-        File[] files = packsDirectory.listFiles((dir, name) -> name.endsWith(".json"));
-        if (files == null || files.length == 0) {
-            p.getLogger().info("no files in packs directory matching filter(?)");
-            return Collections.emptyMap();
+        if (!jsonDirectory.isDirectory()) {
+            p.getLogger().severe("Pack directory was not a directory? Dropping out!");
         }
 
+        File[] files = jsonDirectory.listFiles((dir,name) -> name.endsWith(".json"));
         for (File pack : files) {
             try {
                 FileReader reader = new FileReader(pack);
-                EmojiSet set = gson.fromJson(reader, EmojiSet.class);
-                for (EmojiSet.SpriteMeta emoji : set.emojis) {
-                    emoji.emojiText = emoji.emojiText.toLowerCase().strip();
+                try {
+                    CustomEmojiSet set = gson.fromJson(reader, CustomEmojiSet.class);
+                    for (CustomEmojiSet.SpriteMeta emoji : set.emojis) {
+                        emoji.emojiText = emoji.emojiText.toLowerCase().strip();
+                    }
+                    emoteSet.put(set.prefix, set);
+                } catch (Exception e) {
+                    p.getLogger().severe("Pack parsing error for pack " + pack.getName());
+                    p.getLogger().severe(e.getMessage());
                 }
-                map.put(set.prefix, set);
+
             } catch (FileNotFoundException e) { // we shouldnt generally EXPECT to end up here, but for now we just
                 throw new RuntimeException(e);  // wrap it.
             }
         }
-        return Collections.unmodifiableMap(map);
+        return emoteSet;
     }
 
-    private static Map<Emoji, ObjectComponent> createBaseEmojiSet() {
-        return EmojiManager.getAllEmojis().stream().map(e ->
-                Map.entry(e, Component.object(ObjectContents.sprite(
-                        Key.key("paintings"),
-                        Key.key("unicode/" + StringUtils.joinWith("-", e.getHtmlHexadecimalCode().replace("&#x", "").split(";")).toLowerCase())
-                )))
-        ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
-
-
-    protected SpriteEmojiManager(Minemoji p, Map<String, EmojiSet> customPacks) {
+    public SpriteEmojiManager(Minemoji p, Map<String, CustomEmojiSet> customEmotes) {
         this.p = p;
-        p.getLogger().info("Found following packs:" + String.join(", ", customPacks.keySet()));
+        this.customEmoteMap.putAll(customEmotes);
         if (p.getConfig().getBoolean("unicode-emojis.enabled", true)) {
-            this.emojiMap = new DualHashBidiMap<>(createBaseEmojiSet());
-        } else { this.emojiMap = new DualHashBidiMap<>(); }
+            String uriString = p.getConfig().getString("unicode-emojis.uri");
+            if (uriString != null) {
+                try {
+                    URI packURI = URI.create(uriString);
+                    unicodeEmojiSet = new UnicodeEmojiSet(packURI);
+                } catch (IllegalArgumentException e) {
+                    p.getLogger().info("Invalid URI. Unicode will not be created.");
+                }
+            } else {
+                p.getLogger().info("No uri provided for Unicode. Your emotes should be bundled in another resource pack!");
+                unicodeEmojiSet = new UnicodeEmojiSet(null);
+            }
 
-        this.customPacks = customPacks;
-        this.customSprites = new DualHashBidiMap<>(customPacks.entrySet().stream()
-                .flatMap(e ->
-                        e.getValue().emojis.stream()
-                                .map(meta -> Map.entry(
-                                        meta.emojiText,
-                                        Key.key(meta.resource)
-                                ))
-                ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
-        );
-
-        this.customSpriteDiscord = customPacks.entrySet().stream()
-                .flatMap(e -> e.getValue().emojis.stream()
-                        .map(m -> Map.entry(
-                                Key.key(m.resource),
-                                m.toDiscordString()
-                        )))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
+        }
     }
 
-    public static ObjectComponent spriteMetaToComponent(EmojiSet.SpriteMeta meta) {
-        return Component.object(ObjectContents.sprite(
-                Key.key("paintings"),
-                Key.key(meta.resource)
-        ));
+    public final HashMap<String, CustomEmojiSet> customEmoteMap = new HashMap<>();
+    public Optional<EmojiSet> findEmojiSet(String family) {
+        if (family.equalsIgnoreCase("unicode")) { return Optional.ofNullable(unicodeEmojiSet); }
+        return Optional.ofNullable(customEmoteMap.get(family));
     }
-
-    public Map<Emoji,ObjectComponent> getDefaultEmojiMap() { return Collections.unmodifiableMap(emojiMap); }
-    public Map<String, EmojiSet> getCustomPacks() { return Collections.unmodifiableMap(customPacks); }
-
-    private Optional<ObjectComponent> getEmojiFromActual(Emoji emoji) {
-        return Optional.ofNullable(emojiMap.get(emoji));
+    public Optional<EmojiSet> findEmojiSet(Key emoteKey) {
+        return findEmojiSet(StringUtils.substringBefore(emoteKey.value(), '/'));
     }
-
-    public Optional<String> getCustomEmojiBySprite(Key sprite) {
-        return Optional.ofNullable(customSprites.getKey(sprite));
-    }
-    public Optional<String> getCustomEmojiDiscordRepresentation(Key sprite) {
-        return Optional.ofNullable(customSpriteDiscord.get(sprite));
-    }
-    public Optional<ObjectComponent> getCustomEmoji(String key) {
-        return Optional.ofNullable(customSprites.get(key))
-                .map(sprite -> Component.object(ObjectContents.sprite(
-                        Key.key("paintings"),
-                        sprite
-                )));
+    public Optional<EmojiSet.SpriteMeta> findEmoji(Key emoteKey) {
+        return findEmojiSet(emoteKey).flatMap(set -> set.tryFindByKey(emoteKey));
     }
 
     /// Text replacer that replaces unicode sequences with the corresponding emoji sprite
@@ -173,7 +138,7 @@ public class SpriteEmojiManager {
             .replacement((result, b) -> {
                 String match = result.group();
                 return EmojiManager.getEmoji(match)
-                        .flatMap(emoji -> getEmojiFromActual(emoji).map(c -> (Component) c))
+                        .map(e -> (Component) unicodeEmojiSet.findByEmote(e).toObjectComponent())
                         .orElse(Component.text(match));
             })
             .build();
@@ -181,85 +146,60 @@ public class SpriteEmojiManager {
     ///  Text replacer that replaces prefixed emotes :identifier--sprite: with their corresponding emoji
     ///  (has to search rather than map lookup)
     private final TextReplacementConfig PREFIXED_EMOTE_REPLACER = TextReplacementConfig.builder()
-            .match(Pattern.compile(":(([a-zA-Z0-9_]*)--([a-zA-Z0-9_~]*)):"))
-            .replacement((result, b) -> {
-                if (result.group(2).trim().equalsIgnoreCase("unicode")) {
-                    return EmojiManager.getByDiscordAlias(":" + result.group(3) + ":")
-                            .flatMap(emoji -> getEmojiFromActual(emoji).map(c -> (Component) c))
-                            .orElse(Component.text(result.group()));
-                } else {
-                    return getNamespacedCustomEmoji(result.group(1).trim().toLowerCase()).map(c -> (Component) c)
-                            .orElse(Component.text(result.group()));
-                }
+            .match(Pattern.compile(":([a-zA-Z0-9_]+)--([a-zA-Z0-9_~-]+):"))
+            .replacement((result, b) ->
+                    findEmojiSet(result.group(1).trim())
+                            .flatMap(set -> set.tryFindByText(result.group(2)).map(meta -> (Component) meta.toObjectComponent()))
+                            .orElse(Component.text(result.group()))
+            ).build();
 
-            }).build();
-
-    ///  Text replacer that replaces custom or default emotes according to their name/alias.
+    ///  Text replacer that replaces custom emotes.
     ///  As per how the alias and sprite lookups are, if there is a conflict of names, whichever came in last
     ///  gets the spot. Default emotes should override all emotes.
     private final TextReplacementConfig DEFAULT_EMOTE_REPLACER = TextReplacementConfig.builder()
-            .match(":([a-zA-Z0-9_~]*):")
-            .replacement((result, b) -> {
-                String match = result.group(1).toLowerCase().trim();
-                return getCustomEmoji(match).map(c -> (Component) c)
-                        .orElse(EmojiManager.getByDiscordAlias(result.group()) // try to get emoji by alias
-                                .flatMap(this::getEmojiFromActual)
-                                .map(c -> (Component) c)
-                                .orElse(Component.text(result.group()))
-                        );
-            })
-            .build();
+            .match(":([a-zA-Z0-9_~-]+):")
+            .replacement((result, b) -> customEmoteMap.values().stream()
+                    .flatMap(set -> set.tryFindByText(result.group(1).toLowerCase().trim())
+                            .map(EmojiSet.SpriteMeta::toObjectComponent)
+                            .map(c -> (Component) c)
+                            .stream()
+                    ).findFirst()
+                    .or(() -> EmojiManager.getByDiscordAlias(":" + result.group(1).toLowerCase().trim() +":")
+                                    .flatMap(e -> unicodeEmojiSet != null
+                                            ? Optional.of(unicodeEmojiSet.findByEmote(e))
+                                            : Optional.empty()
+                                    ).map(EmojiSet.SpriteMeta::toObjectComponent)
+                    )
+                    .orElse(Component.text(result.group()))
+            ).build();
 
-    protected Optional<Emoji> getEmojiByComponent(ObjectComponent component) {
-        return Optional.ofNullable(emojiMap.getKey(component));
-    }
-    ///  doesnt touch the children. If its an ObjectComponent with a matching sprite key, it will be turned into a
-    ///  TextComponent. if it doesnt have a matching key or otherwise, it is left UNTOUCHED!
-    public Component demojizeSingleComponent(Component component, boolean discord) {
-        if (component instanceof ObjectComponent o && o.contents() instanceof SpriteObjectContents s) {
-            // getPackByPrefix(s.sprite().value().split("/",2)[0])
-            //                    .flatMap(e -> e.emojis.stream()
-            //                            .filter(emoji -> Objects.equals(emoji.resource, s.sprite().value()))
-            //                            .findFirst()
-            //                    )
-            //                    .map(EmojiSet.SpriteMeta::toDiscordString)
-            return (discord ? getCustomEmojiDiscordRepresentation(s.sprite())
-                    : getCustomEmojiBySprite(s.sprite()).map(str -> ":" + str + ":"))
-                    .map(str -> (Component) Component.text(str))
-                    .orElse(getEmojiByComponent(o)
-                            .map(e -> e.getDiscordAliases().getFirst())
-                            .map(str -> (Component) Component.text(str))
-                            .orElse(component) // just use the old one
-                    );
-        }
-        return component;
-    }
-
-    public Component demojize(Component component, boolean discord) {
-        var childs = component.children().stream().map(c -> demojizeSingleComponent(c,discord)).collect(Collectors.toList());
-        return demojizeSingleComponent(component,discord).children(childs);
-    }
     public Component emojize(Component component) {
-        // try to replace Unicode text emotes
-        return component.replaceText(UNICODE_REPLACER)
+        Component firstStage = unicodeEmojiSet != null
+                ? component.replaceText(UNICODE_REPLACER)
+                : component;
+
+        return firstStage
                 .replaceText(PREFIXED_EMOTE_REPLACER)
                 .replaceText(DEFAULT_EMOTE_REPLACER);
     }
 
-    protected Optional<EmojiSet> getPackByPrefix(String prefix) {
-        return Optional.ofNullable(customPacks.get(prefix));
+
+    public Component demojize(Component component) {
+        var children = component.children().stream()
+                .map(this::demojizeSingleComponent)
+                .toList();
+
+        return demojizeSingleComponent(component).children(children);
     }
-    protected Optional<ObjectComponent> getNamespacedCustomEmoji(String identifier) {
-        // search a pack for an emoji rather than pucking out of an alias map.
-        // requires format :packName--emoji: !
-        var _arr =identifier.split("--", 2);
-        String packIdentifier = _arr[0];
-        String spriteName = _arr[1];
-        return getPackByPrefix(packIdentifier)
-                .flatMap(set -> set.emojis.stream()
-                        .filter(emote -> emote.emojiText.equalsIgnoreCase(spriteName))
-                        .findFirst()
-                         .map(SpriteEmojiManager::spriteMetaToComponent)
-                );
+
+    Component demojizeSingleComponent(Component component) {
+        if (!(component instanceof ObjectComponent oc && oc.contents() instanceof SpriteObjectContents soc)) {
+            return component;
+        }
+
+        return findEmoji(soc.sprite())
+                .map(m -> (Component) Component.text(m.toDiscordString()))
+                .orElse(component);
     }
+
 }
